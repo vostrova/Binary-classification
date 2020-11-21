c978eac5057d00540de5ffbdefe71816d23c8005
vostrova <vostrova92@gmail.com>
2020-11-21 17:14:39 +0100

    Update word_class_classification_with_NN.py

--- word_class_classification_with_NN.py
+++ word_class_classification_with_NN.py
@@ -128,7 +128,7 @@ model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train_onehot_np, train_y_np, epochs=10, validation_split=0.2, verbose=0)
+model.fit(train_onehot_np, train_y_np, epochs=10, validation_split=0.2, verbose=2)
 
 test_onehot_np = np.array(test_onehot)
 test_y_np = np.array(test_y)
